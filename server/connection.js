b102df6fa5491f2d5e790fb0210a64fb586032da
regiejoyo <reginaldjoyo@gmail.com>
2023-02-10 16:43:48 +0800

    adding author and keywords

--- server/connection.js
+++ server/connection.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import http from "http";
 import { app } from "./server.js";
-import { infoLogger } from "./utils/logger.js";
+import { infoLogger, errorLogger } from "./utils/logger.js";
 
 const server = http.createServer(app);
 const PORT = process.env.port;
